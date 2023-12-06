de362bb908b69f4c0f0b3d0c15b806c7eecfaa47
= <=>
2023-12-07 01:14:36 +0530

    sad

--- run_program.sh
+++ run_program.sh
@@ -4,6 +4,6 @@ echo "gnome-terminal needs to be install in order too run this project";
 
 sudo apt-get install gnome-terminal;
 
-sudo gnome-terminal -- bash -c "./runVideo.sh; exec bash" & 
-sudo gnome-terminal -- bash -c "./VideoChat/runChat.sh; exec bash"
+sudo gnome-terminal -- bash -c "./VideoChat/runVideo.sh; exec bash" & 
+sudo gnome-terminal -- bash -c "./runChat.sh; exec bash"
 
